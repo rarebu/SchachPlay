1a8dd68206d1edfbe07c9f747a7caa98cb774797
mboger <marko.boger@gmail.com>
2017-11-05 16:00:28 +0100

    Game works completely with Bootstrap

--- app/controllers/SudokuController.scala
+++ app/controllers/SudokuController.scala
@@ -9,23 +9,44 @@ import de.htwg.se.sudoku.controller.controllerComponent.GameStatus
 @Singleton
 class SudokuController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val gameController = Sudoku.controller
-  def sudokuAsText =  gameController.gridToString + GameStatus.message(gameController.gameStatus)
+  def message = GameStatus.message(gameController.gameStatus)
+  def sudokuAsText =  gameController.gridToString + message
 
   def about= Action {
     Ok(views.html.index())
   }
 
   def sudoku = Action {
-    Ok(views.html.sudoku(gameController))
+    Ok(views.html.sudoku(gameController, message))
   }
 
   def newGrid = Action {
     gameController.createNewGrid
-    Ok(views.html.sudoku(gameController))
+    Ok(views.html.sudoku(gameController, message))
+  }
+
+  def solve = Action {
+    gameController.solve
+    Ok(views.html.sudoku(gameController, message))
+  }
+
+  def highlight(index:Int)= Action {
+    gameController.highlight(index)
+    Ok(views.html.sudoku(gameController, message))
   }
 
   def resize(size:Int)= Action {
     gameController.resize(size)
-    Ok(views.html.sudoku(gameController))
+    Ok(views.html.sudoku(gameController, message))
+  }
+
+  def set(row:Int, col:Int, value:Int)= Action {
+    gameController.set(row,col, value)
+    Ok(views.html.sudoku(gameController, message))
+  }
+
+  def showCandidates(row:Int, col:Int)= Action {
+    gameController.showCandidates(row,col)
+    Ok(views.html.sudoku(gameController, message))
   }
 }
