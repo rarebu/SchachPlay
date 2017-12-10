97daba0e799ebbf634637090f0fee29d835e339f
mboger <marko.boger@gmail.com>
2017-12-11 00:32:18 +0100

    Added a sudoku-field component and got it to work

--- public/javascripts/sudokuVue.js
+++ public/javascripts/sudokuVue.js
@@ -3,6 +3,18 @@ for(let index=1; index <=9; index++){
     sudokuHighlightButtons.push({text: index, link: "/highlight/"+index})
 }
 
+let sudokuHouses = [cells(0),cells(1),cells(2),cells(3),cells(4),cells(5),cells(6),cells(7),cells(8)]
+
+
+function cells(house) {
+    let sudokuCells = []
+    for (let cell = 0; cell < 9; cell++) {
+        sudokuCells.push({house: house, cell: cell, scalar: "scalar" + toScalar(house, cell)})
+    }
+    return sudokuCells
+}
+
+
 $(document).ready(function () {
 
     var sudokuVueMenu = new Vue({
@@ -21,6 +33,22 @@ $(document).ready(function () {
 
 
 
+function toScalar(house, cell) {
+    return house*size + cell;
+}
+
+function row(scalar) {
+    return (Math.floor((scalar % size) /blocksize)) + (blocksize * Math.floor((scalar /(size*blocksize))));
+}
+
+function col(scalar) {
+    return (scalar %blocksize) + (blocksize *Math.floor((scalar/size))) - (size*Math.floor((scalar/(size*blocksize))));
+}
+
+function cell(houseIndex, cellIndex) {
+    return row(toScalar(houseIndex,cellIndex)),col(toScalar(houseIndex,cellIndex))
+}
+
 Vue.component('sudoku-highlight-button-bar', {
     template:`
         <div class="buttonbarcontainer">
@@ -36,7 +64,24 @@ Vue.component('sudoku-highlight-button-bar', {
         return {
             menuItems: sudokuHighlightButtons
         }
-    },
+    }
 
 })
 
+Vue.component('sudoku-field', {
+    template:`
+        <div class="gamecontainer">
+            <div class="game">
+                <div v-for="house in houses" class="house size9">
+                    <div v-for="cell in house" class="cell" v-bind:id="cell.scalar"></div>
+                </div>
+            </div>
+        </div>
+    `,
+    data: function () {
+        return {
+            houses: sudokuHouses
+        }
+    },
+
+})
